bec25ff20a331498c599b408bd3962c2dd3f1473
Decodam <arghyamondal.work@gmail.com>
2024-09-01 23:18:31 +0530

    auth ui made

---
+++ src/app/login/page.jsx
@@ -0,0 +1,10 @@
+import AuthForm from "@/auth/form";
+
+
+export default function Home({}) {
+  return (
+    <div className="min-h-svh flex justify-center items-center sm:px-4 py-14">
+      <AuthForm />
+    </div>
+  );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,9 +1,13 @@
+import Link from "next/link";
 
 
 export default function Home({}) {
   return (
     <div>
       Home
+      <Link href={"/login"}>
+        Login to get started
+      </Link>
     </div>
   );
 }
